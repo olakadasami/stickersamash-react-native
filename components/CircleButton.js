3ba2e942c8c8d061fba571b050752692166879d3
Samuel Olakada <olakadasamuel@gmail.com>
2023-05-02 22:44:07 +0100

    chore: added icon button, and add emoji button

---
+++ components/CircleButton.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Pressable, StyleSheet, View } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+function CircleButton({ onPress }) {
+  return (
+    <View style={styles.circleButtonContainer}>
+      <Pressable onPress={onPress} style={styles.circleButton}>
+        <MaterialIcons name="add" size={38} color={"#25292e"} />
+      </Pressable>
+    </View>
+  );
+}
+
+export default CircleButton;
+
+const styles = StyleSheet.create({
+  circleButtonContainer: {
+    width: 84,
+    height: 84,
+    marginHorizontal: 60,
+    borderWidth: 4,
+    borderColor: "#ffd33d",
+    borderRadius: 42,
+    padding: 3,
+  },
+  circleButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 42,
+    backgroundColor: "#fff",
+  },
+});
